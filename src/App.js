5b3781e7a5c26eea1969b04a8408e95351f69fe5
erkindunya <erkindunya@gmail.com>
2018-12-18 16:38:39 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MovieBrowser from './modules/movie-browser/movie-browser.container';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 class App extends Component {
   render() {
     return (
-      // Provides the Material UI theme to child components
-      <MuiThemeProvider>
-        <MovieBrowser />
-      </MuiThemeProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
 
 export default App;
-
-  
