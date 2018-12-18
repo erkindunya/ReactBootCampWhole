58b5071954f1c73e3552a120c83da85d52a60625
erkindunya <erkindunya@gmail.com>
2018-12-18 16:24:35 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './PhotoList.js';
+import React, { Fragment } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+import FetchApproach from './FetchApproach';
+import AxiosApproach from './AxiosApproach';
+import AsyncAwaitApproach from './AsyncAwaitApproach';
+import HigherOrderComponentApproach from './HigherOrderComponentApproach';
+import RenderPropApproach from './RenderPropApproach';
 
-    this.state = {
-      hits: [],
-      isLoading: false,
-      error: null,
-    };
-  }
-      componentDidMount() {
-        fetch('https://api.mydomain.com')
-          .then(response => response.json())
-          .then(data => this.setState({ data }));
-      }
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () =>
+  <Fragment>
+    <RenderPropApproach />
+  </Fragment>
+  // <FetchApproach />
+  // <AxiosApproach />
+  // <AsyncAwaitApproach />
+  // <HigherOrderComponentApproach />
+  // <RenderPropComponentApproach />
 
 export default App;
