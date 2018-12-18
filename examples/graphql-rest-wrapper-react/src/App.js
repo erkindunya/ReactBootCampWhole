f8e009c7e85988d10e7035f92b1ecb7c413bd314
erkindunya <erkindunya@gmail.com>
2018-12-18 17:12:45 +0000

    Add files via upload

---
+++ examples/graphql-rest-wrapper-react/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class App extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div className="App">
+        { console.log(data) }
+      </div>
+    );
+  }
+}
+
+
+// log dat
+export const FETCH_MOVIE = gql`
+  query {
+    movie(id:550){
+      id,title,poster_path,
+      credits{
+        cast{
+          name,character,profile_path
+        }
+      },
+      rating{
+        avg_rating,total_ratings
+      },
+      recommendations{
+        results{
+          title,poster_path
+        }
+      }
+    }
+  }
+`
+const fetchMovie = graphql(FETCH_MOVIE)
+
+export default fetchMovie(App);
